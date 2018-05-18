66b220552a2867d9a74f908783907a0e529e12eb
sksergey <skovorodkinsergey86@gmail.com>
2018-05-18 17:23:45 +0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
